78341ddf6f6f19e1f31084c2249dd685dc802064
Hamza <m_hamzak1997@yahoo.com>
2019-03-08 17:45:04 +0500

    8/3/19 TODO 32 done

--- app/src/main/java/com/example/a2minut/githubsearch/MainActivity.java
+++ app/src/main/java/com/example/a2minut/githubsearch/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.a2minut.githubsearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_url, tv_search_results;
+    EditText et_searchbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_url = findViewById(R.id.tv_url);
+        tv_search_results = findViewById(R.id.tv_search_results);
+        et_searchbox = findViewById(R.id.et_searchbox);
     }
 }
